62a726d0e109281bbb93934d2ca0007714b675c7
Aleksey Denisov <alde.owl@gmail.com>
2018-12-15 22:58:47 +0700

    add maket

--- src/index.js
+++ src/index.js
@@ -4,23 +4,8 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-function Hello() {
-    let str = 'world'
-    return(
-        <h1> Hello {str}!</h1>
-    )
-}
 
-function SayFullName(props) {
-    return(
-        <div>
-            <h1> My name {props.name}, surname {props.surname}</h1>
-            <a href={props.link}>Ссылка на мой профиль</a>
-        </div>
-    )
-}
-
-ReactDOM.render(<SayFullName name='Alex' surname='Denisov' link='www.google.com' />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 
 serviceWorker.unregister();
